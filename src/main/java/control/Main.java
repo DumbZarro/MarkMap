4593d83c752e5a7bc6a2d5a840595d9c1843785c
ZARRO <129870756@qq.com>
2021-05-13 01:04:16 +0800

    十二点几啦!做楞啊做!饮茶先啦

--- src/main/java/control/Main.java
+++ src/main/java/control/Main.java
@@ -25,31 +25,9 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
-
-
-//        MapNode parentNode = treeService.getRootNode();
-//        // 创建新的节点,以及找到父节点
-//        Integer parentId = treeService.getTree().getRootId();
-//        MapNode sonNode0 = new MapNode(2);
-//        MapNode sonNode1 = new MapNode(3);
-//        MapNode sonNode2 = new MapNode(4);
-//        MapNode gransonNode =  new MapNode(5);
-//        MapNode gransonNode1 =  new MapNode(6);
-//        sonNode0.setContent("11111");
-//        sonNode1.setContent("22222");
-//        sonNode2.setContent("33333");
-//
-//        // 添加节点
-//        nodeService.addNode(parentId,sonNode0.getId(),sonNode0);
-//        nodeService.addNode(parentId,sonNode1.getId(),sonNode1);
-//        nodeService.addNode(parentId,sonNode2.getId(),sonNode2);
-//        nodeService.addNode(sonNode2.getId(),gransonNode.getId(),gransonNode);
-
-
         // 重新计算坐标
         treeService.getTree().setLayout("right");
         nodeService.changeNodeSize(100);//TODO 放缩
-//        nodeService.addNode(sonNode2.getId(),gransonNode1.getId(),gransonNode1);
         treeService.updateLayout();
         generator = new Generator(nodeService,treeService,root);
 
